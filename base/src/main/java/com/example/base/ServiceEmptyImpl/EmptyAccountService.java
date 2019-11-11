751d67f4be7f5b1b7ef4f272a2aa15dfedcf0c11
W9004521 <wanghan5950@163.com>
2019-11-11 10:38:25 +0800

    update

--- base/src/main/java/com/example/base/ServiceEmptyImpl/EmptyAccountService.java
+++ base/src/main/java/com/example/base/ServiceEmptyImpl/EmptyAccountService.java
@@ -1,5 +1,11 @@
 package com.example.base.ServiceEmptyImpl;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentManager;
+
+import com.example.base.base.BaseFragment;
 import com.example.base.service.IAccountService;
 
 public class EmptyAccountService implements IAccountService {
@@ -11,6 +17,11 @@ public class EmptyAccountService implements IAccountService {
 
     @Override
     public String getAccountId() {
+        return null;
+    }
+
+    @Override
+    public BaseFragment newUserFragment(Activity activity, int containerId, FragmentManager manager, Bundle bundle, String tag) {
         return null;
     }
 }
